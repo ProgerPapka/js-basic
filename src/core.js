f0b1a5dd07ca76068a7afacbc113e621b9345afd
Sergey <fpsm2504@gmail.com>
2023-10-10 18:14:36 +0500

    10

--- src/core.js
+++ src/core.js
@@ -1,23 +1,64 @@
 //Напишите функцию, которая проверяет, является ли число целым используя побитовые операторы
-function isInteger(n) {}
+function isInteger(n) {
+    return n === (n ^ 0);
+}
 
 //Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
-function even() {}
+function even() {
+    var numbers = [];
+    for (var i = 2; i <= 20; i += 2) {
+        numbers.push(i);
+    }
+    return numbers;
+}
 
 //Напишите функцию, считающую сумму чисел до заданного используя цикл
-function sumTo(n) {}
+function sumTo(n) {
+    var sum = 0;
+    for (var i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+}
 
 //Напишите функцию, считающую сумму чисел до заданного используя рекурсию
-function recSumTo(n) {}
+function recSumTo(n) {
+    if (n == 1) {
+        return 1;
+    }
+    return n + recSumTo(n - 1);
+}
 
 //Напишите функцию, считающую факториал заданного числа
-function factorial(n) {}
+function factorial(n) {
+    if (n == 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
 
 //Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
-function isBinary(n) {}
+function isBinary(n) {
+    if (n == 0) {
+        return false;
+    }
+    while (n > 1) {
+        if (n % 2 == 1) {
+            return false;
+        }
+        n = n / 2;
+    }
+    return true;
+}
 
 //Напишите функцию, которая находит N-е число Фибоначчи
-function fibonacci(n) {}
+function fibonacci(n) {
+    if (n < 2) {
+        return n;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
 
 /** Напишите функцию, которая принимает начальное значение и функцию операции
  * и возвращает функцию - выполняющую эту операцию.
